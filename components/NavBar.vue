b4769348ac57f15dc9cccef3564ad282a2310145
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2020-04-11 09:21:50 +0200

    customize for My Training Log

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="nav">
-    <nuxt-link to="/" class="brand">Real World Events</nuxt-link>
+    <nuxt-link to="/" class="brand">My Training Log</nuxt-link>
     <nav>
       <nuxt-link to="/">List</nuxt-link> |
-      <nuxt-link to="/create">Create</nuxt-link>
+      <nuxt-link to="/create">Create</nuxt-link> |
+      <nuxt-link to="/one-rm">1RM</nuxt-link>
     </nav>
   </div>
 </template>
